47e2c0630a29c3c481fc704c1c935ef0446cbdd7
Yosu26 <yosu7013@gmail.com>
2025-04-06 00:37:05 +0800

    Modify file

--- data_setup.py
+++ data_setup.py
@@ -3,35 +3,32 @@ import os
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader 
 
-NUM_WORKER = os.cpu_count() 
+# NUM_WORKER = os.cpu_count()
 
 def create_dataloaders(
   train_dir: str,
   test_dir: str,
   transform: transforms.Compose,
   batch_size: int,
-  num_workers: int=NUM_WORKER
 ):
 
-train_data = datasets.ImageFolder(train_dir, transform=transform)
-test_data = datasets.ImageFolder(test_dir, transform=transform)
+  train_data = datasets.ImageFolder(train_dir, transform=transform)
+  test_data = datasets.ImageFolder(test_dir, transform=transform)
 
-class_names = train_data.classes
+  class_names = train_data.classes
 
-train_dataloader = DataLoader(
-  train_data,
-  batch_size=batch_size,
-  num_workers=num_workers,
-  shuffle=True,
-  pin_memory=True
-)
+  train_dataloader = DataLoader(
+    train_data,
+    batch_size=batch_size,
+    shuffle=True,
+    pin_memory=True
+  )
 
-test_dataloader = DataLoader(
-  test_data,
-  batch_size=batch_size,
-  num_workers=num_workers,
-  shuffle=False,
-  pin_memory=True
-)
+  test_dataloader = DataLoader(
+    test_data,
+    batch_size=batch_size,
+    shuffle=False,
+    pin_memory=True
+  )
 
-return train_dataloader, test_dataloader, class_names
+  return train_dataloader, test_dataloader, class_names
