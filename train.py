328a09b66daa357a4b920c19312c2c0f2a081d74
Yosu26 <yosu7013@gmail.com>
2025-04-06 23:26:42 +0800

    modify cudnn settings to resolve local training issue on Windows

--- train.py
+++ train.py
@@ -71,6 +71,9 @@ if __name__ == '__main__':
 
   device = "cuda" if torch.cuda.is_available() else "cpu"
 
+  torch.backends.cudnn.benchmark = False
+  torch.backends.cudnn.deterministic = True
+
   data_transform = transforms.Compose([
     transforms.Resize(size=(64, 64)),
     transforms.ToTensor()
