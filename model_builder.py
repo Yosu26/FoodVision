0df8658962a802a5f319b303472cb55d492f6572
Yosu26 <yosu7013@gmail.com>
2025-04-06 23:46:10 +0800

    add docstrings to functions

--- model_builder.py
+++ model_builder.py
@@ -2,6 +2,16 @@
 from torch import nn
 
 class TinyVGG(nn.Module):
+  """Creates the TinyVGG architecture.
+
+  Replicates the TinyVGG architecture from the CNN explainer website in PyTorch.
+  See the original architecture here: https://poloclub.github.io/cnn-explainer/
+
+  Args:
+    input_shape: An integer indicating number of input channels.
+    hidden_units: An integer indicating number of hidden units between layers.
+    output_shape: An integer indicating number of output units.
+  """
   def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
     super().__init__()
     self.conv_block1 = nn.Sequential(
