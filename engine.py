b667c0babdbe8b7c2c4881ba5e6b451d2efa1615
Yosu26 <yosu7013@gmail.com>
2025-04-13 00:27:40 +0800

    Fixing a minor bug

--- engine.py
+++ engine.py
@@ -67,17 +67,17 @@ def test_step(
   model.eval()
 
   test_loss, test_acc = 0, 0
+  with torch.inference_mode():
+    for batch, (X, y) in enumerate(dataloader):
+      X, y = X.to(device), y.to(device)
 
-  for batch, (X, y) in enumerate(dataloader):
-    X, y = X.to(device), y.to(device)
+      test_pred = model(X)
 
-    test_pred = model(X)
+      loss = loss_fn(test_pred, y)
+      test_loss += loss 
 
-    loss = loss_fn(test_pred, y)
-    test_loss += loss 
-
-    test_pred_labels = torch.argmax(test_pred, dim=1)
-    test_acc += ((test_pred_labels == y).sum().item()/len(test_pred))
+      test_pred_labels = torch.argmax(test_pred, dim=1)
+      test_acc += ((test_pred_labels == y).sum().item()/len(test_pred))
 
   test_loss = test_loss / len(dataloader)
   test_acc = test_acc / len(dataloader)
