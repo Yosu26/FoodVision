7adcd63d3e425c029e91d4b863bf53ed3a434665
Yosu26 <yosu7013@gmail.com>
2025-04-14 15:13:44 +0800

    add function to create ViT model with PyTorch

--- model_builder.py
+++ model_builder.py
@@ -1,4 +1,5 @@
-
+import torch
+import torchvision
 from torch import nn
 
 class TinyVGG(nn.Module):
@@ -38,3 +39,33 @@ class TinyVGG(nn.Module):
     x = self.conv_block2(x)
     x = self.classifier(x)
     return x
+  
+def create_vit_model(num_classes:int=3,
+                     seed:int=42):
+  """Creates an ViTB16 feature extractor model and transforms.
+
+    Args:
+        num_classes (int, optional): number of classes in the classifier head. 
+            Defaults to 3.
+        seed (int, optional): random seed value. Defaults to 42.
+
+    Returns:
+        model (torch.nn.Module): ViTB16 feature extractor model. 
+        transforms (torchvision.transforms): EffNetB2 image transforms.
+  """
+  # Create ViTB16 pretrained weights, transforms and model
+  weights = torchvision.models.ViT_B_16_Weights.DEFAULT
+  transforms = weights.transforms()
+  model = torchvision.models.vit_b_16(weights=weights)
+
+  # Freeze all layers in base model
+  for param in model.parameters():
+      param.requires_grad = False 
+
+  # Change classifier head with random seed for reproducibility
+  torch.manual_seed(seed)
+  model.classifier = nn.Sequential(
+      nn.Linear(in_features=768, out_features=num_classes)
+  )
+
+  return model, transforms
